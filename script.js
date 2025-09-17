890b8402ad79f32e51963a2a22d835423736df5d
shubham somavamshi <shubhamraopatil123@gmail.com>
2025-09-17 11:53:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function toFahrenheit(celsius) {
- // Write your code here
+ return (9/5 * celsius + 32).toFixed(2);
 }
 
 // Do not change the code below
 const celsius = prompt("Enter Celsius:");
-alert(toFahrenheit(Number(celsius)));
+alert(toFahrenheit(Number(celsius))); 
